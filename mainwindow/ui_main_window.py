964dbd153f7f9d8c978a9acf1bb1a554d585f621
Vitor Eduardo <vitoreduardo@pop-os.localdomain>
2023-02-04 20:06:53 -0300

    .

--- mainwindow/ui_main_window.py
+++ mainwindow/ui_main_window.py
@@ -12,5 +12,70 @@ class UI_MainWindow(object):
             #------------------------------
 
 
-
+            #///////////////////////////////////////////////////
+            # Setando frame e layout principal da janela
+            self.main_frame = QFrame()
             
+            self.main_layout = QHBoxLayout(self.main_frame)
+            self.main_layout.setContentsMargins(0,0,0,0)
+            self.main_layout.setSpacing(0)
+            #///////////////////////////////////////////////////
+
+
+            #///////////////////////////////////////////////////
+            # Setando barra esquerda, layout e seus widgets
+            self.left_bar = QFrame()
+            self.left_bar.setMinimumWidth(380)
+            self.left_bar.setMaximumWidth(380)
+            self.left_bar.setStyleSheet("background: red")
+
+            self.left_bar_layout = QVBoxLayout(self.left_bar)
+            self.left_bar_layout.setContentsMargins(0,0,0,0)
+            self.left_bar_layout.setSpacing(0)
+
+            self.txt_site = QLineEdit()
+
+            self.txt_email = QLineEdit()
+
+            self.txt_senha = QLineEdit()
+
+            self.btn_pass_gen = QPushButton("Gerar Senha")
+            self.lbl_site = QLabel()
+            self.lbl_email = QLabel()
+            self.lbl_senha = QLabel()
+
+            self.meu_layout = QFormLayout()
+            self.meu_layout.addRow("Site/App", self.txt_site)
+            
+
+            self.left_bar_layout.addItem(self.meu_layout)
+            #///////////////////////////////////////////////////
+            
+            
+
+
+            #///////////////////////////////////////////////////
+            # Setando barra direita, layout e seus widgets
+            self.right_bar = QFrame()
+            self.right_bar.setStyleSheet("background: blue")
+
+            self.right_bar_layout = QVBoxLayout(self.right_bar)
+            self.right_bar_layout.setContentsMargins(0,0,0,0)
+            self.right_bar_layout.setSpacing(0)
+
+
+
+
+            #///////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+            self.main_layout.addWidget(self.left_bar)
+            self.main_layout.addWidget(self.right_bar)
+
+            parent.setCentralWidget(self.main_frame)
